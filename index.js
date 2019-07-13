8e3158ea348184d67915f1b1d58279d27ac3edbd
Michał Nowakowski <nowakowskimichal1988@gmail.com>
2019-07-13 18:39:02 +0200

    Add new files

---
+++ index.js
@@ -0,0 +1,16 @@
+var fs = require('fs');
+var StatMode = require('stat-mode');
+var colors = require('colors');
+
+fs.readdir('../17.7', 'utf-8', function(err, data){
+	console.log(data);
+	var lsData = data;
+	fs.writeFile('./ls-file.txt', lsData, function(err) {
+		if (err) throw err;
+        console.log('Zapisano!'.blue);
+        fs.readFile('./ls-file.txt', 'utf-8', function(err, data) {
+            console.log('Dane po zapisie'.blue)
+            console.log(data);
+        });
+	});
+});
